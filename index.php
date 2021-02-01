a5bd2b4040d4eefa6bd7818cbf76afc2f4b85685
Julien_Deveaux <julien.deveaux@hotmail.com>
2021-02-01 11:46:48 +0100

    Affichage principal des prix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	$titre = "Accueil";
 	include 'debutSkelXhtml.php';
 		echo '<h1>Accueil</h1>';
-		echo '<p><a href="prix.php">Prix</a></p>';
+		echo '<p><a href="page_prix.php">Prix</a></p>';
 		echo '<p><a href="films.php">Films</a></p>';
 		echo '<p><a href="recipiendaires.php">Recipiendaire</a></p>';
 		echo '<p><a href="categories.php">Catégories</a></p>';
