59357949b133c7c58065526605749c8cc024b5d1
Julien_Deveaux <julien.deveaux@hotmail.com>
2021-02-01 10:38:55 +0100

    démarrage

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	$titre = "Accueil";
+	include 'debutSkelXhtml.php';
+		echo '<h1>Accueil</h1>';
+		echo '<p><a href="prix.php">Prix</a></p>';
+		echo '<p><a href="films.php">Films</a></p>';
+		echo '<p><a href="recipiendaires.php">Recipiendaire</a></p>';
+		echo '<p><a href="categories.php">Catégories</a></p>';
+
+    include 'finKtml.html';
+?>
