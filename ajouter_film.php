e3c85ae8c43a4fc3686b2f0c20a90a5eaae982fe
Justine Lemesle <justine.lemesle2000@gmail.com>
2021-02-18 18:24:23 +0100

    ajout de la liste des recipiendaire

--- ajouter_film.php
+++ ajouter_film.php
@@ -2,6 +2,8 @@
 $titre = "Ajouter un film";
 include 'debutSkelXhtml.php';
 include 'Film.php';
+include 'Recipiendaire.php';
+include 'Concerne.php';
 
 echo '<form action="ajouter_film.php" method="get" >';
 $film = Film::initFilm(1);
@@ -15,8 +17,14 @@ if(isset ($_GET['titre_film'])){
     $film->setNouveau(true);
     $film->setTitre_film($_GET['titre_film']);
     $film->setTitre_original($_GET['titre_original']);
+
+    $concerne = Concerne::initConcerne_recipiendaire($_GET['reci']);
+    $concerne->setid_film($film->getId_film());
+
+    //$concerne->save();
     $film->save();
-    //echo $film;
+    echo $concerne;
+    echo $film;
     echo  '<meta http-equiv="refresh" content="0;URL=page_film.php" />';
 } else {
     echo '<h1>Ajout d\'un film</h1>';
@@ -24,6 +32,15 @@ if(isset ($_GET['titre_film'])){
     echo 'Titre : <input type="text" name="titre_film" size=50 required>';
     echo '<br/>';
     echo 'Titre original : <input type="text" name="titre_original" size=50 required>';
+    echo '<br/>';
+    echo 'Récipiendaire qui à réalisé ce film :';
+    $recipiendaireAll = Recipiendaire::getAll();
+    $comboBox = '<select name="reci">';
+    for($i = 0; $i < sizeof($recipiendaireAll); $i++) {
+        $comboBox .= '<option value="'.$recipiendaireAll[$i]->getid_recipiendaire().'">'.$recipiendaireAll[$i]->getnom_recipiendaire().' '.$recipiendaireAll[$i]->getprenom_recipiendaire().'</option>';
+    }
+    $comboBox .= '</select>';
+    echo $comboBox;
     echo '<br/>';
 
     echo '<button type="submit" name="valider" > Valider </button>';
