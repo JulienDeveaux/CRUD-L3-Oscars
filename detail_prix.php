39498617f79610745bccc899bbf25d8d0fb22559
Julien_Deveaux <julien.deveaux@hotmail.com>
2021-02-18 18:16:22 +0100

    affichage en liste de la liste des cérémonies qui ont accueilli le prix + pareil pour recipiendaire

--- detail_prix.php
+++ detail_prix.php
@@ -13,29 +13,44 @@
 		if(is_numeric($_GET['id'])){
 			$prix = Prix::initPrix(intval($_GET['id']));
 			if(!empty($prix)){
-				$ceremonie = Ceremonie::initCeremonie($prix->getid_prix());
+			    $chararray = str_split($prix->getnom_prix());
+			    for($i = 4; $i < sizeof($chararray); $i++) {
+			        $chararray[$i] = '';
+                }
+			    $string = implode($chararray);
+				$ceremonie = Ceremonie::getCeremonieByNom($string);
 				echo '<h1>'.$prix->getnom_prix().'</h1>';
 				echo '<img width=100 height=150 alt="'.$prix->getid_prix().'" src="Illustrations/Prix/'.$prix->getid_prix().'.png">';
 				echo '</br>';
-				echo '<h1> A été organisé à : '.$ceremonie->getnom_ceremonie().'</h1>';
+				echo '<h1> Cérémonies de ce prix :</h1>';
 
 				if(!empty($ceremonie)){
-					echo '<ul>';
-					$lien = '';
-					$lien .= '<li><a href="detail_ceremonie.php?id='.$ceremonie->getid_ceremonie().'"><img width=70 height=70 alt="'.$ceremonie->getnom_ceremonie().'" src="Illustrations/Ceremonie/'.$ceremonie->getid_ceremonie().'.png"></a>'.$ceremonie->getnom_ceremonie().' </li>';
+                    echo '<ul>';
+                    $lien = '';
+				    for($i = 0; $i < sizeof($ceremonie); $i++) {
+                        $lien .= '<li><a href="detail_ceremonie.php?id='.$ceremonie[$i]->getid_ceremonie().'"><img width=70 height=70 alt="'.$ceremonie[$i]->getnom_ceremonie().'" src="Illustrations/Ceremonie/'.$ceremonie[$i]->getid_ceremonie().'.png"></a>'.$ceremonie[$i]->getnom_ceremonie().' </li>';
+                    }
 					$lien .= '</ul>';
 					echo $lien;
-				}
-				$organisation = Organisation::initOrganisation($prix->getid_organisation());
-				echo '<h1>Organisation : '.$organisation->getnom_organisation().'</h1>';
-				echo '<h2>Type de l\'organisation : '.$organisation->gettype_organisation().'</h2>';
+				} else {
+				    echo '<h1><font color=orange>Aucune cérémonie pour ce prix</font></h1>';
+                }
+                try {
+                    $organisation = Organisation::initOrganisation($prix->getid_organisation());
+				    echo '<h1>Organisation : '.$organisation->getnom_organisation().'</h1>';
+				    echo '<h2>Type de l\'organisation : '.$organisation->gettype_organisation().'</h2>';
+                } catch (Exception $e) {
+				    echo '<h1><font color=orange>Aucune Organisation pour ce Prix trouvé</font></h1>';
+                }
 
 				echo '<h1>Récipiendaires qui ont obtenus ce prix : </h1>';
-				$categorie = Categorie::initCategorie_prix($prix->getid_prix());
-				$nomination = Nomination::initNomination_categorie($categorie->getid_categorie());
-				$concerne = Concerne::initConcerne_nomination($nomination->getid_nomination());
-				$recipiendaire = Recipiendaire::initRecipiendaire($concerne->getid_recipiendaire());
-				echo '<h2>'.$recipiendaire->getnom_recipiendaire().' '.$recipiendaire->getprenom_recipiendaire().'</h2>';
+				$recipiendaire = Recipiendaire::getRecipiendaireListPrix($prix->getid_prix());
+                $lien = '<ul>';
+				for($i = 0; $i < sizeof($recipiendaire); $i++) {
+				    $lien .= '<li><a href="detail_recipiendaire.php?id='.$recipiendaire[$i]->getid_recipiendaire().'"><img width=70 height=70 alt="'.$recipiendaire[$i]->getnom_recipiendaire().'" src="Illustrations/Recipiendaire/'.$recipiendaire[$i]->getid_recipiendaire().'.png"></a> '.$recipiendaire[$i]->getnom_recipiendaire().' '.$recipiendaire[$i]->getprenom_recipiendaire().'</li>';
+                }
+				$lien .= '</ul>';
+				echo $lien;
 			}
 		} else {
 			// Erreur : id incorrecte renseigné dans l'url : cas non atteint en navigation normale sur le site
